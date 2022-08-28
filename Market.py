4e3533ad906d561c353be14a76ee91b5efc48e33
supernova-Z313 <supernova313.313z@gmail.com>
2022-08-28 17:01:42 +0430

    user object init and 0 to 2 manager

--- Market.py
+++ Market.py
@@ -105,4 +105,182 @@ class Market:
 
 
 # =================================================================
+class User:
+    def __init__(self, name, code, birth, parent: Market, balance: float = 0):
+        self.name = name
+        self.code = code
+        self.birth = birth
+        self.balance = balance
+        self.data = parent.data
+        self.date = parent.date
+        self.date_index = parent.date_index
+        self.currency_balance = {}
+    # -------------------------------------------------------------------------
 
+    def work_station(self):
+        command = None
+        while command != 7:
+            os.system("clear")  # change to cls
+            print(f"Welcome to the home page                   Date:{self.date[0]}\n")
+            print("What do you want to do? \n"
+                  " + Buy a currency                                  (0)\n"
+                  " + Sell a currency                                 (1)\n"
+                  " + Deposit to account                              (2)\n"
+                  " + See market price changes                        (3)\n"
+                  " + View assets and account balances                (4)\n"
+                  " + View chart of price changes                     (5)\n"
+                  " + Linear regression chart of price changes        (6)\n"
+                  " + Exit from your profile                          (7)")
+            command = int(input())
+            # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+            if command == 0:
+                day_data = {"0": ["Symbol",	"Open",	"Volume"]}
+                for ind, i in enumerate(self.date_index, start=1):
+                    i = self.data.iloc[i]
+                    day_data[f"{ind}"] = [i["Symbol"], i["Open"], i["Volume"]]
+                com = None
+
+                while com != 0:
+                    os.system("clear")  # change to cls
+                    print(f"Market state:                                         Date:{self.date[0]}")
+                    print(pd.DataFrame(day_data), end="\n\n")
+                    print(f"Your Balance account:                                             {self.balance}")
+                    com = int(input("To buy any currency, enter its number in the list or enter 0 to exit:\n"))
+                    if com == 1:
+                        amount = float(input("amount of this currency:   "))
+                        while (amount * float(day_data["1"][1]) > self.balance) or (amount * float(day_data["1"][1]) > float(day_data["1"][2])):
+                            amount = float(input("Operation is not possible, please try again.\n"
+                                                 "amount of this currency:    "))
+                        if day_data["1"][0] in self.currency_balance:
+                            self.currency_balance[day_data["1"][0]] += amount
+                        else:
+                            self.currency_balance[day_data["1"][0]] = amount
+                        self.balance -= amount * float(day_data["1"][1])
+                        print("The purchase was successful.")
+                        time.sleep(1.5)
+                    elif com == 2:
+                        amount = float(input("amount of this currency:   "))
+                        while (amount * float(day_data["2"][1]) > self.balance) or (amount * float(day_data["2"][1]) > float(day_data["2"][2])):
+                            amount = float(input("Operation is not possible, please try again.\n"
+                                                 "amount of this currency:    "))
+                        if day_data["2"][0] in self.currency_balance:
+                            self.currency_balance[day_data["2"][0]] += amount
+                        else:
+                            self.currency_balance[day_data["2"][0]] = amount
+                        self.balance -= amount * float(day_data["2"][1])
+                        print("The purchase was successful.")
+                        time.sleep(1.5)
+                    elif com == 3:
+                        amount = float(input("amount of this currency:   "))
+                        while (amount * float(day_data["3"][1]) > self.balance) or (amount * float(day_data["3"][1]) > float(day_data["3"][2])):
+                            amount = float(input("Operation is not possible, please try again.\n"
+                                                 "amount of this currency:    "))
+                        if day_data["3"][0] in self.currency_balance:
+                            self.currency_balance[day_data["3"][0]] += amount
+                        else:
+                            self.currency_balance[day_data["3"][0]] = amount
+                        self.balance -= amount * float(day_data["3"][1])
+                        print("The purchase was successful.")
+                        time.sleep(1.5)
+                    elif com == 4:
+                        amount = float(input("amount of this currency:   "))
+                        while (amount * float(day_data["4"][1]) > self.balance) or (amount * float(day_data["4"][1]) > float(day_data["4"][2])):
+                            amount = float(input("Operation is not possible, please try again.\n"
+                                                 "amount of this currency:    "))
+                        if day_data["4"][0] in self.currency_balance:
+                            self.currency_balance[day_data["4"][0]] += amount
+                        else:
+                            self.currency_balance[day_data["4"][0]] = amount
+                        self.balance -= amount * float(day_data["4"][1])
+                        print("The purchase was successful.")
+                        time.sleep(1.5)
+                    elif com == 5:
+                        amount = float(input("amount of this currency:   "))
+                        while (amount * float(day_data["5"][1]) > self.balance) or (amount * float(day_data["5"][1]) > float(day_data["5"][2])):
+                            amount = float(input("Operation is not possible, please try again.\n"
+                                                 "amount of this currency:    "))
+                        if day_data["5"][0] in self.currency_balance:
+                            self.currency_balance[day_data["5"][0]] += amount
+                        else:
+                            self.currency_balance[day_data["5"][0]] = amount
+                        self.balance -= amount * float(day_data["5"][1])
+                        print("The purchase was successful.")
+                        time.sleep(1.5)
+
+            # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+            elif command == 1:
+                day_data = {"0": ["Symbol",	"Open",	"Volume"]}
+                for ind, i in enumerate(self.date_index, start=1):
+                    i = self.data.iloc[i]
+                    day_data[f"{ind}"] = [i["Symbol"], i["Open"], i["Volume"]]
+                com = None
+
+                if len(self.currency_balance) == 0:
+                    os.system("clear")  # change to cls
+                    input("You have no currency to sell, please buy some first.")
+                    com = 0
+                while com != 0:
+                    currency_data_frame = {0: ["Symbol", "Amount", "Volume"]}
+                    z_a = []
+                    for ind, i in enumerate(self.currency_balance, start=1):
+                        adel = None
+                        for j in self.date_index:
+                            j = self.data.iloc[j]
+                            if j["Symbol"] == i:
+                                adel = j["Open"]
+                                z_a.append(adel)
+                                break
+                        currency_data_frame[ind] = [i, self.currency_balance[i], self.currency_balance[i] * adel]
+                    os.system("clear")  # change to cls
+                    print(f"Market state:                                         Date:{self.date[0]}")
+                    print(pd.DataFrame(day_data), end="\n\n")
+                    print("Account state:    ")
+                    print(pd.DataFrame(currency_data_frame), end="\n\n")
+                    print(f"Your Balance account:                                        {self.balance}")
+                    com = int(input("To sell any currency, enter its number in the list or enter 0 to exit:\n"))
+                    if com == 1:
+                        amount = float(input("amount of this currency:    "))
+                        while amount > currency_data_frame[1][1]:
+                            amount = float(input("Operation is not possible, please try again.\n"
+                                                 "amount of this currency:    "))
+                        self.currency_balance[currency_data_frame[1][0]] -= amount
+                        self.balance += z_a[0] * amount
+                        print("Currency sold successfully.")
+                        time.sleep(2)
+                    elif com == 2:
+                        amount = float(input("amount of this currency:    "))
+                        while amount > currency_data_frame[2][1]:
+                            amount = float(input("Operation is not possible, please try again.\n"
+                                                 "amount of this currency:    "))
+                        self.currency_balance[currency_data_frame[2][0]] -= amount
+                        self.balance += z_a[1] * amount
+                        print("Currency sold successfully.")
+                        time.sleep(2)
+                    elif com == 3:
+                        amount = float(input("amount of this currency:    "))
+                        while amount > currency_data_frame[3][1]:
+                            amount = float(input("Operation is not possible, please try again.\n"
+                                                 "amount of this currency:    "))
+                        self.currency_balance[currency_data_frame[3][0]] -= amount
+                        self.balance += z_a[2] * amount
+                        print("Currency sold successfully.")
+                        time.sleep(2)
+                    elif com == 4:
+                        amount = float(input("amount of this currency:    "))
+                        while amount > currency_data_frame[4][1]:
+                            amount = float(input("Operation is not possible, please try again.\n"
+                                                 "amount of this currency:    "))
+                        self.currency_balance[currency_data_frame[4][0]] -= amount
+                        self.balance += z_a[3] * amount
+                        print("Currency sold successfully.")
+                        time.sleep(2)
+                    elif com == 5:
+                        amount = float(input("amount of this currency:    "))
+                        while amount > currency_data_frame[5][1]:
+                            amount = float(input("Operation is not possible, please try again.\n"
+                                                 "amount of this currency:    "))
+                        self.currency_balance[currency_data_frame[5][0]] -= amount
+                        self.balance += z_a[4] * amount
+                        print("Currency sold successfully.")
+                        time.sleep(2)
+            # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
